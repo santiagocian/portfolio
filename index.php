db2f39b54d86243a129d902935862f7fa89e2f55
santiagocian <ciansantiago15@gmail.com>
2018-05-02 16:01:11 +0800

    folio

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Cian Santiago</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+
+
+		<style>
+			.body{
+				background-color: white;
+			}
+		</style>
+
+
+	</head>
+	<body class="body">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Wrapper -->
+					<div id="wrapper">
+
+						<!-- Panel (Banner) -->
+							<section class="panel banner right">
+								<div class="content color0 span-3-75">
+									<h1 class="major">CIAN<br/>
+									SANTIAGO</h1>
+									<p><b>Front-end Developer and Web designer</b></p>
+									<ul class="actions">
+										<li><a href="#first" class="button special color1 circle icon fa-angle-right">Next</a></li>
+									</ul>
+								</div>
+								<div class="image filtered span-1-75" data-position="25% 25%">
+									<img src="assets/images/CIAN.jpg" alt="" />
+								</div>
+							</section>
+
+						<!-- Panel (Spotlight) -->
+							<section class="panel spotlight medium right" id="first">
+								<div class="content span">
+									<h2 class="major">About Me</h2>
+									<p>My name is Patricia Ann "Cian" Santiago. I'm an aspiring web developer living in Manila. I took up Business Administration major in Management. Work as a Graphic Artist year 2017.</p>
+								</div>
+								<div class="image filtered tinted" data-position="top left">
+									<img src="images/pic02.jpg" alt="" />
+								</div>
+							</section>
+
+						<!-- Panel -->
+							<section class="panel color1">
+								<div class="intro joined">
+									<h2 class="major">Skills</h2>
+									<p></p>
+								</div>
+								<div class="inner">
+									<ul class="grid-icons">
+										<li>
+										Beginner
+											<ul>
+												<li>Adobe Premiere</li>
+												<li>Adobe Lightroom</li>
+												<li>Sketchup</li>
+
+
+											</ul>
+										</li>
+										<li>Intermidiate
+											<ul>
+												<li>HTML5</li>
+												<li>CSS3</li>
+												<li>Javascript</li>
+												<li>PHP</li>
+												<li>Mysql</li>
+												<li>Laravel</li>
+												<li>Adobe Illustrator</li>
+												<li>Photography</li>
+											</ul>
+										</li>
+										<li>Advance
+											<ul>
+												<li>Adobe Photoshop</li>
+
+											</ul>
+										</li>
+
+
+									</ul>
+								</div>
+							</section>
+					
+
+						<!-- Panel -->
+							<section class="panel">
+								<div class="intro color2">
+									<h2 class="major">Works</h2>
+									<p></p>
+								</div>
+								<div class="gallery">
+									<a href="" class="image filtered span-2-5" data-position="top" onclick="capstoneOne()" alt="Capstone 1" ><img src="assets/images/04.jpg" alt="Capstone 1" /></a>
+									<div class="group span-4-5">
+										<a href="" class="image filtered span-3" data-position="top" onclick="capstoneTwo()" alt="Capstone 2" ><img src="assets/images/05.jpg" alt="" /></a>
+										<a href="" class="image filtered span-1-5" data-position="center"><img src="assets/images/06.jpg" alt="" /></a>
+										<a href="https://tinyurl.com/papsntgresume" target="_blank" class="image filtered span-1-5" data-position="bottom"><img src="assets/images/07.jpg" alt="" /></a>
+										<a href="" class="image filtered span-3" data-position="top" onclick="capstoneThree()" alt="Capstone 3" ><img src="assets/images/08.jpg" alt="" /></a>
+									</div>
+									<a href="" class="image filtered span-2-5" data-position="right" onclick="photography()" alt="Photography" ><img src="assets/images/09.jpg" alt="" /></a>
+								</div>
+							</section>
+
+						<!-- Panel -->
+							<section class="panel color4-alt">
+								<div class="intro color4">
+									<h2 class="major">Contact</h2>
+									<p>Patricia Ann Santiago</p>
+									<p>San Roque St. Sta Ana Manila</p>
+									<p>+63917-806-8296</p>
+									<p>papsntg@gmail.com</p>
+
+								</div>
+								<div class="inner columns divided">
+									<div class="span-1-5">
+										<ul class="contact-icons color1">
+											<li class="icon fa-facebook"><a href="https://www.facebook.com/cian2015">facebook.com/cian2015</a></li>
+											<li class="icon fa-instagram"><a href="https://www.instagram.com/aquarelle.mnl/">@aquarelle.mnl</a></li>
+											<li class="icon fa-gitlab"><a href="https://gitlab.com/santiagocian">@santiagocian</a></li>
+										</ul>
+									</div>
+								</div>
+							</section>
+
+
+						<!-- Copyright -->
+							<div class="copyright">&copy; Cian Santiago</div>
+
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/main.js"></script>
+
+			<script>
+			    function capstoneOne() {
+			        window.open("http://creativenav.x10host.com/Index.html");
+			    }
+			    function capstoneTwo() {
+			        window.open("http://cnsntg.x10host.com/");
+			    }
+			    function capstoneThree() {
+			        window.open("https://thatblissfulday.000webhostapp.com/");
+			    }
+			    function photography() {
+			        window.open("http://cnsntg.x10host.com/");
+			    }
+
+
+			</script>
+
+	</body>
+</html>
